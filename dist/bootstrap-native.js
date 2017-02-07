25d7660ae9308ebd4948f8690732c89b540eaa14
mz <mattgeoc@autistici.org>
2017-02-07 11:20:43 +0100

    Change stuff with size of draggable modal box + update dist files

--- dist/bootstrap-native.js
+++ dist/bootstrap-native.js
@@ -65,12 +65,20 @@
     tipPositions = /\b(top|bottom|left|top)+/;
   
   var makeDraggable = function(element){
-      if(!element)
-          element = !element ? document.querySelectorAll(".draggable-box")
-                  : typeof element == "string" ? document.querySelectorAll(element)
-                  : typeof element == "object" && element.length == 1 ? [element]
-                  : typeof element == "object" ? element : [];
+      element = !element ? document.querySelectorAll(".draggable-box")
+              : typeof element == "string" ? document.querySelectorAll(element)
+              : typeof element == "object" && element.length == 1 ? [element]
+              : typeof element == "object" ? element : [];
       var nb_draggable = element.length;
+  
+    	function prepare_position(el){
+        	var child_modal_dialog = el.childNodes[0],
+              bbox_modal = child_modal_dialog.getBoundingClientRect();
+      		child_modal_dialog.style.margin = "0";
+          el.style.width = bbox_modal.width + "px"; el.style.height = bbox_modal.height + "px";
+      		el.style.left = window.innerWidth / 2 - bbox_modal.width / 2 + "px";
+      		el.style.top = bbox_modal.top < 0 ? "10px" : bbox_modal.top + "px";
+      }
   
       function startDrag(event) {
           event.preventDefault();
@@ -78,16 +86,8 @@
           var parent_box = this.parentElement.parentElement.parentElement,
               child_modal_dialog = parent_box.childNodes[0],
               bbox_modal = child_modal_dialog.getBoundingClientRect(),
-              alreadyDragged = child_modal_dialog.getAttribute("dragged"),
               diffX = event.layerX,
               diffY = event.layerY;
-  
-          if(!alreadyDragged){
-              child_modal_dialog.setAttribute("dragged", true);
-              child_modal_dialog.style.margin = "0";
-              parent_box.style.left = window.innerWidth / 2 - bbox_modal.width / 2 + "px";
-              parent_box.style.top = bbox_modal.y + "px";
-          }
   
           function move(event) {
               event.preventDefault();
@@ -116,6 +116,7 @@
       }
   
       for(var i = 0; i < nb_draggable; i++){
+          prepare_position(element[i]);
           var el = element[i].querySelector(".modal-header") || element[i];
           el.addEventListener("mousedown", startDrag);
       }
